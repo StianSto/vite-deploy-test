841bab39a4f0a60ad3dcd9b5970575286005a1c1
Stian Stordal <stordalstian@gmail.com>
2023-01-06 18:53:22 +0100

    rm vite logo

--- src/js/main.mjs
+++ src/js/main.mjs
@@ -1,14 +1,10 @@
 import "../css/style.css";
 import "../scss/style.scss";
 import javascriptLogo from "../javascript.svg";
-import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.mjs";
 
 document.querySelector("#app").innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
